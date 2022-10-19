27502f25832d795186e355716ecfb1c63e625fea
Roman Sukhopleshchenko <roman.sukhopleshchenko@yandex.ru>
2022-10-19 11:30:32 +0300

    fix: remove export of const user/name

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export const user = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
+const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
 
-export function sayHi(name) {
+export default function sayHi() {
   console.log(`Hello, ${name}!`);
 }
